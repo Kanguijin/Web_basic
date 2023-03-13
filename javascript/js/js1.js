55d23c3606a1c0a6a6f9c7cfda0258850e31e2df
Kanguijin <wheretogobro@naver.com>
2023-03-13 16:50:13 +0900

    css 계속

---
+++ javascript/js/js1.js
@@ -0,0 +1,14 @@
+// alert("알림창을 확인하세요");
+// alert("두번째 알림창");
+// alert(10 + 20);
+// console.log("println");
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+let test = 10;
+console.log(test);
+
+let value1 = parseInt(prompt("첫번째 숫자를 입력해주세요"));
+let value2 = parseInt(prompt("두번째 숫자를 입력해주세요"));
+console.log(value1+value2);
